fcd8b0ffab9bbcc6fbb4bb9fe8e94b1e4ce1a5f8
Sander Wågsæther Egenæs <sanderwaags1@hotmail.com>
2021-09-06 17:43:39 +0200

    Added ConvertOneTo1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Main {
+    @Test
+    public void convertOneToI() {
+        assertEquals("I", romanNumbers(1));
+    }
+
+    private String romanNumbers(int Number) {
+       return "" ;
+    }
 }
